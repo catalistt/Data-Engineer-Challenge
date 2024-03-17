924a38b1f34439238736dd5c1a522b1fb6ce5f5b
catalistt <caliste@fen.uchile.cl>
2024-03-17 12:58:59 -0300

    Fix: q1_memory dict added

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,42 +1,35 @@
 from typing import List, Tuple
 from datetime import datetime
+from collections import defaultdict
 import create_json_array
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    # Obtener la lista de tweets del archivo
+ # Obtener la lista de tweets del archivo
     tweet_list = create_json_array.create_json_array(file_path)
 
-    # Inicializar diccionario para almacenar los recuentos de tweets por fecha y usuario
-    tweet_counts = {}
+    # Inicializar un diccionario para almacenar los recuentos de tweets por fecha y usuario
+    tweet_counts = defaultdict(lambda: defaultdict(int))
 
-    # Recorrer los tweets y contar los tweets por fecha y usuario
+    # Inicializar un diccionario para almacenar el recuento de tweets por fecha
+    date_counts = defaultdict(int)
+
+    # Iterar los tweets y contar los tweets por fecha y usuario
     for tweet in tweet_list:
         # Obtener la fecha del tweet y el nombre de usuario
         date_element = datetime.strptime(tweet['date'], '%Y-%m-%dT%H:%M:%S+00:00').date()
         username_element = tweet['user']['username']
 
-        # Actualizar el recuento de tweets para esta fecha y usuario
-        if date_element in tweet_counts:
-            if username_element in tweet_counts[date_element]:
-                tweet_counts[date_element][username_element] += 1
-            else:
-                tweet_counts[date_element][username_element] = 1
-        else:
-            tweet_counts[date_element] = {username_element: 1}
+        # Actualizar los recuentos de tweets para esta fecha y usuario
+        tweet_counts[date_element][username_element] += 1
+        date_counts[date_element] += 1
 
-    # Ordenar las fechas por cantidad de tweets desc
-    sorted_dates = sorted(tweet_counts.items(), key=lambda x: sum(x[1].values()), reverse=True)
+    # Obtener las top 10 fechas con más tweets
+    sorted_dates = sorted(date_counts.items(), key=lambda x: x[1], reverse=True)[:10]
 
-    # Obtener las top 10 fechas con mas tweets
-    top_10_dates = sorted_dates[:10]
-
-    # Inicializar lista para almacenar los resultados finales
+    # Obtener el usuario con más publicaciones para cada una de las top 10 fechas
     top_10_result_list = []
-
-    # Recorrer las top 10 fechas y obtener el usuario con más publicaciones para cada una
-    for date, user_counts in top_10_dates:
-        # Obtener el usuario con más publicaciones para esta fecha
-        top_user = max(user_counts, key=user_counts.get)
+    for date, _ in sorted_dates:
+        top_user = max(tweet_counts[date], key=tweet_counts[date].get)
         top_10_result_list.append((date, top_user))
 
     return top_10_result_list
