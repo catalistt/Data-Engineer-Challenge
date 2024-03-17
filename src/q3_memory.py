87ace51f736d09e2daf248766843e42fe98acce7
catalistt <caliste@fen.uchile.cl>
2024-03-16 22:40:14 -0300

    Adding: q3_memory function logic

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,36 @@
 from typing import List, Tuple
+from collections import Counter
+import create_json_array 
+import re
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # Inicializar listas vacias para almacenar tweets y contador de tags
+    tweets_content = []
+    user_counter = []
+    flat_user_counter = []
+
+    # Obtener los tweets del archivo y guardar contenido
+    tweet_list = create_json_array.create_json_array(file_path)
+    for tweet in tweet_list:
+        tweets_content.append(tweet['content'])
+
+    # RE para encontrar tags de usuarios
+    re_tags = r'@[^ ]+'
+
+    # Encontrar tags de usuarios en el contenido de los tweets y almacenarlos en user_counter
+    for tweet in tweets_content:
+        mentions = re.findall(re_tags, tweet)
+        if mentions:
+            user_counter.append([mention[1:] for mention in mentions])
+
+    # Aplanar la lista de listas de tags de usuarios en una sola lista
+    for sublist in user_counter:
+        flat_user_counter.extend(sublist)
+
+    # Contar la frecuencia de cada tag de usuario
+    user_counter = Counter(flat_user_counter)
+
+    # Obtener los top 10 usuarios mencionados
+    top_10_users = user_counter.most_common(10)
+
+    return top_10_users
--- src/q3_time.py
+++ src/q3_time.py
@@ -5,20 +5,20 @@ import re
 
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    
+
     # Inicializar contador para contar tags
     user_counter = Counter()
 
-    # Expresión regular para encontrar menciones de usuarios
+    # RE para encontrar tags de usuarios
     re_tags = re.compile(r'@([^ ]+)')
 
-    # Obtener los tweets del archivo y contar las menciones de usuarios
+    # Obtener los tweets del archivo y contar tags de usuarios
     tweet_list = create_json_array.create_json_array(file_path)
     for tweet in tweet_list:
         if 'content' in tweet:
             # Encontrar todos los tags en el contenido del tweet
             mentions = re_tags.findall(tweet['content'])
-            # Aumentar el recuento de cada usuario mencionado en el tweet 
+            # Aumentar el recuento de cada usuario
             user_counter.update(mentions)
 
     # Obtener los top 10 usuarios mencionados
